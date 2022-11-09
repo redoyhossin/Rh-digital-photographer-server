1096962d1cd229e3c0ab4942924c37d4dcf822f9
redoywebhero <redoyhossin5984@gmail.com>
2022-11-09 18:21:55 +0600

    get data in database

--- index.js
+++ index.js
@@ -16,13 +16,26 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j9rm2ly.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function photographyervice() {
+  try {
+    const servicesCollection = client.db('photographyervice').collection('services ');
+    app.get('/allservices', async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query)
+      const user = await cursor.toArray();
+      res.send(user)
+    });
+    
+  }
+  finally {
+    
+  }
+}
+photographyervice().catch(console.dir);
+
+
 
 
 
