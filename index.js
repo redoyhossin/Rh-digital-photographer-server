0e82a6c871e21e11063d4edc54e2e7996f8752d0
redoywebhero <redoyhossin5984@gmail.com>
2022-11-10 09:31:19 +0600

    get post data review

--- index.js
+++ index.js
@@ -49,7 +49,19 @@ async function photographyervice() {
       const result = await reviewCollection.insertOne(review)
       res.send(result)
       
-})
+    });
+    
+    app.get('/reviews',async (req, res) => {
+      let query={}
+      if(req.query.email){
+      query={
+      email:req.query.email
+      }
+      }const cursor =reviewCollection.find (query)
+      const reviews= await cursor.toArray ();
+      res.send(reviews)
+      
+      })
     
 
 
